07da4cd2d2f8b4da08bef756e3dfd72536690311
Shin Xi <jinshinxi@gmail.com>
2016-02-22 15:44:05 +0800

    clean code at Caregiver.coffee

--- app/view/Caregiver.coffee
+++ app/view/Caregiver.coffee
@@ -3,97 +3,11 @@ Ext.define 'HomeTrak.view.Caregiver',
   xtype: 'caregiver'
   layout: 'fit'
   initComponent: ->
-    searchTreeStore = Ext.create('Ext.data.TreeStore', root:
-      expanded: true
-      children: [
-        {
-          text: 'A'
-          leaf: false
-          children: [
-            {
-              text: 'Albright, Mary(Z)'
-              leaf: true
-            }
-            {
-              text: 'Albright, Atty(A)'
-              leaf: true
-            }
-          ]
-        }
-        {
-          text: 'B'
-          leaf: false
-          children: [
-            {
-              text: 'Bellaire, Marcy(A)'
-              leaf: true
-            }
-            {
-              text: 'Briggs, John(A)'
-              leaf: true
-            }
-            {
-              text: 'Broderick, Betty(A)'
-              leaf: true
-            }
-          ]
-        }
-        {
-          text: 'C'
-          leaf: false
-          children: [
-            {
-              text: 'Clothier, Bert(A)'
-              leaf: true
-            }
-            {
-              text: 'Colhoun, Jessica(A)'
-              leaf: true
-            }
-            {
-              text: 'Colhoun, Jessica(A)'
-              leaf: true
-            }
-          ]
-        }
-        {
-          text: 'D'
-          leaf: true
-        }
-        {
-          text: 'E'
-          leaf: true
-        }
-        {
-          text: 'F'
-          leaf: true
-        }
-        {
-          text: 'G'
-          leaf: true
-        }
-        {
-          text: 'H'
-          leaf: true
-        }
-        {
-          text: 'I'
-          leaf: true
-        }
-      ])
-    searchTree =
-      xtype: 'treepanel'
-      flex: 1
-      store: searchTreeStore
-      frame: false
-      rowLines: true
-      rootVisible: false
-
     l10n = HomeTrak.util.Localization
 
     @items = [
       xtype: 'panel'
-      title: (l10n.get 'caregiver_name') + ': John'
+      title: (l10n.get 'caregiver_name') + ': 张三'
       layout: 
         type: 'hbox'
         align: 'stretch'
--- app/view/Caregiver.js
+++ app/view/Caregiver.js
@@ -4,88 +4,12 @@ Ext.define('HomeTrak.view.Caregiver', {
   xtype: 'caregiver',
   layout: 'fit',
   initComponent: function() {
-    var l10n, searchTree, searchTreeStore;
-    searchTreeStore = Ext.create('Ext.data.TreeStore', {
-      root: {
-        expanded: true,
-        children: [
-          {
-            text: 'A',
-            leaf: false,
-            children: [
-              {
-                text: 'Albright, Mary(Z)',
-                leaf: true
-              }, {
-                text: 'Albright, Atty(A)',
-                leaf: true
-              }
-            ]
-          }, {
-            text: 'B',
-            leaf: false,
-            children: [
-              {
-                text: 'Bellaire, Marcy(A)',
-                leaf: true
-              }, {
-                text: 'Briggs, John(A)',
-                leaf: true
-              }, {
-                text: 'Broderick, Betty(A)',
-                leaf: true
-              }
-            ]
-          }, {
-            text: 'C',
-            leaf: false,
-            children: [
-              {
-                text: 'Clothier, Bert(A)',
-                leaf: true
-              }, {
-                text: 'Colhoun, Jessica(A)',
-                leaf: true
-              }, {
-                text: 'Colhoun, Jessica(A)',
-                leaf: true
-              }
-            ]
-          }, {
-            text: 'D',
-            leaf: true
-          }, {
-            text: 'E',
-            leaf: true
-          }, {
-            text: 'F',
-            leaf: true
-          }, {
-            text: 'G',
-            leaf: true
-          }, {
-            text: 'H',
-            leaf: true
-          }, {
-            text: 'I',
-            leaf: true
-          }
-        ]
-      }
-    });
-    searchTree = {
-      xtype: 'treepanel',
-      flex: 1,
-      store: searchTreeStore,
-      frame: false,
-      rowLines: true,
-      rootVisible: false
-    };
+    var l10n;
     l10n = HomeTrak.util.Localization;
     this.items = [
       {
         xtype: 'panel',
-        title: (l10n.get('caregiver_name')) + ': John',
+        title: (l10n.get('caregiver_name')) + ': 张三',
         layout: {
           type: 'hbox',
           align: 'stretch'
